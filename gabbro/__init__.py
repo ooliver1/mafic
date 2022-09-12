4475195f6e0c6f80a31f5757d3f6685ba21faf11
ooliver1 <oliverwilkes2006@icloud.com>
2022-09-12 22:35:22 +0100

    feat: add basic init file

---
+++ gabbro/__init__.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: MIT
+"""
+Lavalink Client for Python
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+A properly typehinted lavalink client for discord.py, nextcord, disnake and py-cord.
+:copyright: (c) 2022-present ooliver1
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "gabbro"
+__author__ = "ooliver1"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present ooliver1"
+__version__ = "0.1.0"
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
