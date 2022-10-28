8c9d5fd8e9b48381c79d8ffac64a15c28f5b6ddf
ooliver1 <oliverwilkes2006@icloud.com>
2022-10-28 13:16:10 +0100

    feat: add mafic -v for version checking

---
+++ mafic/__main__.py
@@ -0,0 +1,61 @@
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import platform
+import sys
+from argparse import ArgumentParser, Namespace
+
+import aiohttp
+
+import mafic
+
+from .__libraries import library, version_info
+
+VERSION_OUTPUT = """
+- Python v{py.major}.{py.minor}.{py.micro}-{py.releaselevel}
+- mafic v{mafic}
+- aiohttp v{aiohttp}
+- {lib} v{pkg.major}.{pkg.minor}.{pkg.micro}-{pkg.releaselevel}
+- system info: {uname.system} {uname.release} {uname.version}
+""".strip()
+
+
+def show_version():
+    version = mafic.__version__
+    uname = platform.uname()
+    print(
+        VERSION_OUTPUT.format(
+            py=sys.version_info,
+            mafic=version,
+            aiohttp=aiohttp.__version__,
+            uname=uname,
+            lib=library,
+            pkg=version_info,
+        )
+    )
+
+
+def core(_: ArgumentParser, args: Namespace):
+    if args.version:
+        show_version()
+
+
+def parse_args():
+    parser = ArgumentParser(prog="mafic", description="CLI tools for mafic")
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        help="shows mafic and other related versions",
+    )
+    return parser, parser.parse_args()
+
+
+def main():
+    parser, args = parse_args()
+    core(parser, args)
+
+
+if __name__ == "__main__":
+    main()
