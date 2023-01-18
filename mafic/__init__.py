deb5e0d372f39ca254f3905f9de7c7efcc220f1b
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-01-18 23:58:39 +0000

    docs: document init well

--- mafic/__init__.py
+++ mafic/__init__.py
@@ -1,17 +1,12 @@
 # SPDX-License-Identifier: MIT
 """
-Lavalink Client for Python
-~~~~~~~~~~~~~~~~~~~~~~~~~~
+Mafic
+=====
 A properly typehinted lavalink client for discord.py, nextcord, disnake and py-cord.
+
 :copyright: (c) 2022-present ooliver1
 :license: MIT, see LICENSE for more details.
 """
-
-__title__ = "mafic"
-__author__ = "ooliver1"
-__license__ = "MIT"
-__copyright__ = "Copyright 2022-present ooliver1"
-__version__ = "0.2.0"
 
 import logging
 
@@ -26,6 +21,16 @@ from .search_type import *
 from .strategy import *
 from .track import *
 from .warnings import *
+
+__title__ = "mafic"
+__author__ = "ooliver1"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present ooliver1"
+__version__ = "0.2.0"
+"""The current version of mafic, using `PEP 440`_ format.
+
+.. _PEP 440: https://peps.python.org/pep-0440/
+"""
 
 del __libraries
 
