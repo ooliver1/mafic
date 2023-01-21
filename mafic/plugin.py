34016ffc3016d2908bab0e8a9433d105f40b99a1
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-01-21 16:00:10 +0000

    docs: document plugin

--- mafic/plugin.py
+++ mafic/plugin.py
@@ -16,9 +16,32 @@ __all__ = ("Plugin",)
 
 @dataclass(repr=True)
 class Plugin:
+    """Represents a plugin.
+
+    Attributes
+    ----------
+    name:
+        The name of the plugin.
+    version:
+        The version of the plugin.
+    """
+
     name: str
     version: str
 
     @classmethod
     def from_data(cls, data: PluginData) -> Self:
+        """Create a plugin from raw Lavalink data.
+
+        Parameters
+        ----------
+        data:
+            The plugin data.
+
+        Returns
+        -------
+        Plugin:
+            The plugin.
+        """
+
         return cls(name=data["name"], version=data["version"])
