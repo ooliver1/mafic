6e0c2db48819b5528b0e6ab0cdd0e834befa64b7
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-01-22 00:23:00 +0000

    docs: document stats

--- mafic/plugin.py
+++ mafic/plugin.py
@@ -2,21 +2,22 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing_extensions import Self
-
     from .typings import PluginData
 
 
 __all__ = ("Plugin",)
 
 
-@dataclass(repr=True)
 class Plugin:
     """Represents a plugin.
+
+    Parameters
+    ----------
+    data:
+        The raw data to use to create the plugin.
 
     Attributes
     ----------
@@ -26,22 +27,8 @@ class Plugin:
         The version of the plugin.
     """
 
-    name: str
-    version: str
+    __slots__ = ("name", "version")
 
-    @classmethod
-    def from_data(cls, data: PluginData) -> Self:
-        """Create a plugin from raw Lavalink data.
-
-        Parameters
-        ----------
-        data:
-            The plugin data.
-
-        Returns
-        -------
-        Plugin:
-            The plugin.
-        """
-
-        return cls(name=data["name"], version=data["version"])
+    def __init__(self, data: PluginData) -> None:
+        self.name: str = data["name"]
+        self.version: str = data["version"]
--- mafic/stats.py
+++ mafic/stats.py
@@ -18,6 +18,20 @@ __all__ = (
 
 
 class CPUStats:
+    """Represents CPU stats for a node.
+
+    Attributes
+    ----------
+    cores:
+        The number of cores the node has.
+    system_load:
+        The load on the whole system lavalink is on..
+    lavalink_load:
+        The load Lavalink is using.
+    """
+
+    __slots__ = ("cores", "system_load", "lavalink_load")
+
     def __init__(self, payload: CPU) -> None:
         self.cores: int = payload["cores"]
         self.system_load: int = payload["systemLoad"]
@@ -25,6 +39,27 @@ class CPUStats:
 
 
 class MemoryStats:
+    """Represents memory stats for a node.
+
+    Parameters
+    ----------
+    payload:
+        The raw payload to use to create the stats.
+
+    Attributes
+    ----------
+    free:
+        The amount of free memory.
+    used:
+        The amount of used memory.
+    allocated:
+        The amount of allocated memory.
+    reservable:
+        The amount of reservable memory for the node. Set by ``-Xmx`` for Java.
+    """
+
+    __slots__ = ("free", "used", "allocated", "reservable")
+
     def __init__(self, payload: Memory) -> None:
         self.free: int = payload["free"]
         self.used: int = payload["used"]
@@ -33,6 +68,25 @@ class MemoryStats:
 
 
 class FrameStats:
+    """Represents frame stats for a node.
+
+    Parameters
+    ----------
+    payload:
+        The raw payload to use to create the stats.
+
+    Attributes
+    ----------
+    sent:
+        The amount of frames sent.
+    nulled:
+        The amount of frames nulled.
+    deficit:
+        The amount of frames deficit.
+    """
+
+    __slots__ = ("sent", "nulled", "deficit")
+
     def __init__(self, payload: FrameStatsPayload) -> None:
         self.sent: int = payload["sent"]
         self.nulled: int = payload["nulled"]
@@ -40,6 +94,38 @@ class FrameStats:
 
 
 class NodeStats:
+    """Represents stats for a node.
+
+    Parameters
+    ----------
+    data:
+        The raw data to use to create the stats.
+
+    Attributes
+    ----------
+    player_count:
+        The amount of players connected to the node.
+    playing_player_count:
+        The amount of players playing on the node.
+    uptime:
+        The uptime of the node.
+    memory:
+        The memory stats of the node.
+    cpu:
+        The CPU stats of the node.
+    frame_stats:
+        The frame stats of the node.
+    """
+
+    __slots__ = (
+        "cpu",
+        "frame_stats",
+        "memory",
+        "player_count",
+        "playing_player_count",
+        "uptime",
+    )
+
     def __init__(self, data: Stats) -> None:
         self.player_count: int = data["players"]
         self.playing_player_count: int = data["playingPlayers"]
