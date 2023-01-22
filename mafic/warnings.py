febf304f40705b31a184fb16f07a51b00a6b744c
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-01-22 00:33:01 +0000

    docs: document warnings

--- mafic/warnings.py
+++ mafic/warnings.py
@@ -5,6 +5,8 @@ __all__ = ("UnsupportedVersionWarning",)
 
 
 class UnsupportedVersionWarning(UserWarning):
+    """Represents a warning for an unsupported version of Lavalink."""
+
     message: str = (
         "The version of Lavalink you are using is not supported by Mafic. "
         "It should still work but not all features are supported."
