f2f04eb0e9a0646853f0d2b066bca2faa7076f05
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-01-27 19:39:06 +0000

    docs: export ignore lib check env var

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ from typing import Any, Optional
 
 from sphinx.config import Config
 
+os.environ["MAFIC_IGNORE_LIBRARY_CHECK"] = "1"
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
