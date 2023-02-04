9609b74bf3184d96fcdea200284f0cd740d8e12f
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-02-04 14:14:41 +0000

    feat: support plugin search types

--- mafic/search_type.py
+++ mafic/search_type.py
@@ -8,8 +8,37 @@ __all__ = ("SearchType",)
 
 
 class SearchType(Enum):
-    """Represents a search type for Lavalink."""
+    """Represents a search type for Lavalink.
+
+    .. danger::
+
+        Many of these require plugins to be used.
+
+        `LavaSrc <https://github.com/TopiSenpai/LavaSrc>`_:
+            - :attr:`SPOTIFY_SEARCH`
+            - :attr:`SPOTIFY_RECOMMENDATIONS`
+            - :attr:`APPLE_MUSIC`
+            - :attr:`DEEZER_SEARCH`
+            - :attr:`DEEZER_ISRC`
+            - :attr:`YANDEX`
+
+        `DuncteBot <https://github.com/DuncteBot/skybot-lavalink-plugin>`_:
+            - :attr:`SPEECH`
+    """
 
     YOUTUBE = "ytsearch"
     YOUTUBE_MUSIC = "ytmsearch"
     SOUNDCLOUD = "scsearch"
+
+    # Plugins
+
+    # LavaSrc
+    SPOTIFY_SEARCH = "spsearch"
+    SPOTIFY_RECOMMENDATIONS = "sprec"
+    APPLE_MUSIC = "amsearch"
+    DEEZER_SEARCH = "dzsearch"
+    DEEZER_ISRC = "dzisrc"
+    YANDEX = "ymsearch"
+
+    # DuncteBot
+    SPEECH = "speak"
